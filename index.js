b0511607f649afee424858665b61572f934e18f9
Valirgron <adamjamilkhair@gmail.com>
2021-06-17 23:35:23 +0900

    New Update

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function revealMessage(){
     document.getElementById("hiddenMessage").style.display="block";
 }
+function revealMessage2(){
+    document.getElementById("hiddenMessage2").style.display="block";
+}
+function revealMessage3(){
+    document.getElementById("hiddenMessage3").style.display="block";
+}
 
 function countDown(){
     var currenVal = document.getElementById("countDownButton").innerHTML;
