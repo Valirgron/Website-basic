d1e7d6dcb6eaffe4c65869ac30feed92db54e312
Valirgron <adamjamilkhair@gmail.com>
2021-06-18 14:10:57 +0900

    updated

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-function revealMessage(){
-    document.getElementById("hiddenMessage").style.display="block";
-}
 function revealMessage2(){
     document.getElementById("hiddenMessage2").style.display="block";
 }
